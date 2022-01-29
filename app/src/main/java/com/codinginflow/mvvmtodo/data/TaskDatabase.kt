a081e889dc0ea3e35d065224954b4beff095acfb
saptaksengupta <deeps.sengupta@gmail.com>
2022-01-29 12:33:09 +0530

    dependency injection and database module integrated

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDatabase.kt
@@ -0,0 +1,38 @@
+package com.codinginflow.mvvmtodo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.codinginflow.mvvmtodo.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+    abstract fun taskDao(): TaskDao
+
+    class CallBack @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            val dao = database.get().taskDao()
+
+//            launch a scope to access dao suspend functions
+
+            applicationScope.launch {
+                dao.insert(Task("First task to be completed"))
+                dao.insert(Task("Second task to be completed"))
+                dao.insert(Task("Third task to be completed", important = true ))
+                dao.insert(Task("Fourth task to be completed", completed = true))
+                dao.insert(Task("Fifth task to be completed"))
+            }
+
+        }
+    }
+}
